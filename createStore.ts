8955e659a15d0435c6f529f51bd320b816c1ba00
Marius Aabel <marius@aabel.net>
2024-12-30 14:51:23 +0100

    fjerner tre

--- createStore.ts
+++ createStore.ts
@@ -132,47 +132,47 @@ function createCollection<Data extends { id: any }, ExtraController extends obje
         return [model, setModelData] as [Data, (newModel: Data) => void];
     }
 
-    function useQuery(path: string | string[]) {
-        const modelId = selectedModelId;
+    // function useQuery(path: string | string[]) {
+    //     const modelId = selectedModelId;
 
-        // console.log("useQuery() ", path, modelId);
+    //     // console.log("useQuery() ", path, modelId);
 
-        if (!modelId) {
-            console.log("useQuery() called without modelId and no model is selected");
-            return [null, () => { }];
-        }
+    //     if (!modelId) {
+    //         console.log("useQuery() called without modelId and no model is selected");
+    //         return [null, () => { }];
+    //     }
 
-        const model_ = collectionData[findModelIndexById(collectionData, modelId)] || {} as Data;
-        const initialModel = getObjectByKey(model_, path);
-        const [model, setModel] = useState<Data>(initialModel);
+    //     const model_ = collectionData[findModelIndexById(collectionData, modelId)] || {} as Data;
+    //     const initialModel = getObjectByKey(model_, path);
+    //     const [model, setModel] = useState<Data>(initialModel);
 
 
-        useEffect(() => {
+    //     useEffect(() => {
 
-            function handleChange(d: Data[]) {
-                const idx = findModelIndexById(d, modelId);
-                const newModel_ = { ...d[idx] }
-                const newModel = getObjectByKey(newModel_, path);
-                // console.log("useQuery() handleChange() ", newModel.id);
-                if (newModel.id === "c0.0") {
-                    console.log("useQuery() handleChange() equal? ", deepEqual(model, newModel), model, newModel);
-                }
-                if (deepEqual(model, newModel)) return;
+    //         function handleChange(d: Data[]) {
+    //             const idx = findModelIndexById(d, modelId);
+    //             const newModel_ = { ...d[idx] }
+    //             const newModel = getObjectByKey(newModel_, path);
+    //             // console.log("useQuery() handleChange() ", newModel.id);
+    //             if (newModel.id === "c0.0") {
+    //                 console.log("useQuery() handleChange() equal? ", deepEqual(model, newModel), model, newModel);
+    //             }
+    //             if (deepEqual(model, newModel)) return;
 
-                setModel(idx === -1 ? {} as Data : { ...newModel });
-            }
-            eventHandler.subscribe(handleChange);
-            return () => {
-                eventHandler.unsubscribe(handleChange);
-            };
-        }, [modelId]);
+    //             setModel(idx === -1 ? {} as Data : { ...newModel });
+    //         }
+    //         eventHandler.subscribe(handleChange);
+    //         return () => {
+    //             eventHandler.unsubscribe(handleChange);
+    //         };
+    //     }, [modelId]);
 
-        const setModelData = (newModel: Data) => {
-            console.error("useQuery() setModelData() not implemented. get from set field with path from old version");
-        };
+    //     const setModelData = (newModel: Data) => {
+    //         console.error("useQuery() setModelData() not implemented. get from set field with path from old version");
+    //     };
 
-        return [model, setModelData] as [Data, (newModel: Data) => void];
-    }
+    //     return [model, setModelData] as [Data, (newModel: Data) => void];
+    // }
 
     function useSelected() {
         const [sid, setSid] = useState<string | null>(
@@ -204,7 +204,7 @@ function createCollection<Data extends { id: any }, ExtraController extends obje
         useCollection,
         useModel,
         useSelected,
-        useQuery,
+        // useQuery,
         useController: () => mergedController,
         // ...keep the old useData or code but not used...
     };
