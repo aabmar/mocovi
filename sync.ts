38baa4ed9272045ef00041ef0640c45232688e66
Marius Aabel <marius@aabel.net>
2025-02-12 20:15:28 +0100

    fixed bug on sync

--- sync.ts
+++ sync.ts
@@ -23,9 +23,11 @@ const createSync = (
         connected = true;
         isReconnecting = false;
         for (let store of getStores()) {
-            if ((store.syncMode) && store?.syncCallback) {
+            console.log("sync ws.onopen: store.syncMode", store.id, store.syncMode);
+            if ((store.syncMode)) {
                 store.sync = sync;
                 if (store.syncMode === "auto" || store.syncMode === "get") {
+                    console.log("sync ws.onopen: store.fetch()", store.id);
                     store.mergedController.fetch();
                 }
             }
