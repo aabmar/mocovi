105bb5514dc62e09607e2d62107e9262c5cd3a61
Marius Aabel <marius@aabel.net>
2025-07-12 01:50:51 +0200

    Fixed bug on change selected state change

--- useSelected.ts
+++ useSelected.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import useLog from "./logger";
 import { nanoid } from "./nanoid";
 import { Store, UseSelected, UseSelectedReturn } from "./types";
@@ -23,9 +23,10 @@ function createUseSelected<Data extends { id: string }>(store: Store<Data>): Use
 
         dbg("& USE SELECTED ", changed, " STORE: ", store.id, " SELECTED: ", (selected ? selected.id : "NONE"));
 
-        function handleChange(d: Data[]) {
-
+        const handleChange = useCallback((d: Data[]) => {
             const newModel = store.baseController.getSelected();
+
+            // Compare new model with the previously rendered selected model
             dbg("&& 1) HANDLE CHANGE USE SELECTED ", store.id, changed, "Changed: ", newModel !== selected, (selected ? selected.id : "NONE"), (newModel ? newModel.id : "NONE"));
             if (newModel !== selected) {
                 dbg("&& 2) HANDLE CHANGE USE SELECTED ", store.id, changed, "Changed: ", newModel !== selected, (selected ? selected.id : "NONE"), (newModel ? newModel.id : "NONE"), changed);
@@ -34,7 +35,7 @@ function createUseSelected<Data extends { id: string }>(store: Store<Data>): Use
                     return prev + 1;
                 });
             }
-        }
+        }, [store.id, selected]); // Depend on selected to capture the last rendered value
 
         // On mount, we subscribe to the event handler
         // and we unsubscribe on unmount
@@ -43,7 +44,7 @@ function createUseSelected<Data extends { id: string }>(store: Store<Data>): Use
             return () => {
                 store.eventHandler.unsubscribe(handleChange);
             };
-        }, []);
+        }, [handleChange]);
 
 
         // We make a function to set model data so that this hook works like useState for the user.
