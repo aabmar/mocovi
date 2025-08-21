d72a13743404df1d7170a297d998f595126518d0
Marius Aabel <marius@aabel.net>
2025-08-21 13:36:21 +0200

    changed exports

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Map all exports
 
 import createStore from "./createStore";
-import { clearAll } from "./Store";
+import { clearAll, startSync } from "./Store";
 import { diff } from "./util";
 import useHistory from "./useHistory";
 import logger, { setLog } from "./logger";
@@ -14,13 +14,12 @@ import {
     CreateCollectionOptions, CreateController,
     UseController,
     Message, Model, BaseController,
-
     SyncModes, MessageTypes,
 } from "./types";
 
 export {
     createStore, CreateController, BaseController, Model, Persist, clearAll, diff, Store, Sync,
     CreateCollectionOptions, UseController, Message,
-    useHistory, logger, setLog, stopSync,
+    useHistory, logger, setLog, stopSync, startSync,
     SyncModes, MessageTypes, useStore, useController
 };
